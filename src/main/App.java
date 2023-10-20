5994704bcc3ba11f04f2252007796a7b73f87eec
tomastm22 <119524778+tomastm22@users.noreply.github.com>
2023-10-20 01:50:06 +0000

    Pratica 1

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,25 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        int a = 0;
-        return -1;
+
+        for (int i = 0; i < arrayOrdenado.length; i++) {
+            if (arrayOrdenado[i] == num) {
+                return 1;
+            }
+        }
+        return 0;
+
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int maior = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arrayDesordenado.length; i++) {
+            if (arrayDesordenado[i] > maior) {
+                maior = arrayDesordenado[i];
+            }
+        }
+        return maior;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
